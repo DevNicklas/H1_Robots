284d80e406461d78a194aa4e6fd902471c9142d3
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-09-12 13:35:50 +0200

    Add project files.

---
+++ H1_Robots/Model/Microchip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Robots.Model
+{
+    internal class Microchip
+    {
+        private string _name;
+        private Abilities[] _abilities;
+
+        public Microchip(string name, Abilities[] abilities)
+        {
+            _name = name;
+            _abilities = abilities;
+        }
+
+        public enum Abilities
+        {
+            CleanFloor,
+            CleanWindow,
+            ChangeWheels,
+            None
+        }
+    }
+}
---
+++ H1_Robots/Model/Robot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Robots.Model
+{
+    internal class Robot
+    {
+        private string _name;
+        private Microchip _microchip;
+        private string _color;
+        private bool _isBig;
+        private bool _hasWifi;
+        private SoapContainer _soapContainer;
+        private Battery battery;
+        private byte _wheelAmount;
+
+        internal Robot()
+        {
+
+        }
+
+        internal Robot(string name, Microchip microchip, string color, byte wheels, Battery battery)
+        {
+
+        }
+
+        internal Robot(string name, Microchip microchip, string color, byte wheels, SoapContainer soapContainer)
+        {
+            
+        }
+        
+    }
+}
